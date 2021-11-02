8a127e57ccbb24bb50167f7fdd978b390757747f
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 11:11:08 +0200

    adding magnifier

--- src/lib/modals/pImgModal.svelte
+++ src/lib/modals/pImgModal.svelte
@@ -2,18 +2,23 @@
     import {
         _modal_z_index_incrementor
     } from './../../stores/stores';
+    import {Event} from '$lib/utils/js/Event'
+    import {Magnifier} from '$lib/utils/js/Magnifier.js';
+    
     import {
 CLOUDINARY_URL,
     STATIC_BASE
-  } from './../../api/consts';
+    } from './../../api/consts';
     let isModalOpen = false;
     let modal_zIndex = 0;
     export let product = {'title':'loading...', 'image': ''};
-
+    let m, evt;
     export function toggleModal() {
         isModalOpen = !isModalOpen;
         if (isModalOpen) {
             modal_zIndex = 1200 + (++$_modal_z_index_incrementor * 15);
+
+
         }
     }
     export function isOpen() {
@@ -21,13 +26,25 @@ CLOUDINARY_URL,
     }
 
     export function setProduct(p) {
+        
         product = p
+        setTimeout(()=> {
+            evt = new Event(),
+            m = new Magnifier(evt);
+            m.attach({
+                thumb: '#thumb',
+                largeWrapper: 'preview', 
+                zoomable:true,
+                zoom: 1.5,
+                
+            });
+        }, 100)
     }
 </script>
 
 
 
-<div style="z-index: {modal_zIndex};" id="productModal" class="modal" class:active={isModalOpen}>
+<div style="z-index: {modal_zIndex};" id="productImageModal" class="modal" class:active={isModalOpen}>
     <div style="z-index: {modal_zIndex+5};" class="overlay" on:click={toggleModal}></div>
     <div style="z-index: {modal_zIndex+10};" class="modal_content">
         <div class="modal-header">
@@ -35,11 +52,25 @@ CLOUDINARY_URL,
         </div>
 
         <div class="modal-body">
-            <a  target="_blank" href="{CLOUDINARY_URL}f_auto,w_auto/{product['cimage']}">
-                <div class="img-wraper" style="background-image: url('{CLOUDINARY_URL}f_auto,w_auto/{product['cimage']}');">
-                    
+
+            {#key product}
+                <div class="content">
+                    <!--<a class="magnifier-thumb-wrapper demo" href="#http://en.wikipedia.org/wiki/File:Starry_Night_Over_the_Rhone.jpg">-->
+                    <div class="original-img">
+                        <img alt="ge" id="thumb" src="{CLOUDINARY_URL}f_auto,w_auto/{product['cimage']}"
+                        data-large-img-url="{CLOUDINARY_URL}f_auto,w_auto/{product['cimage']}"
+                        data-large-img-wrapper="preview">
+                    </div>
+                    <!-- </a>-->
+                    <div class="magnifier-preview example heading" id="preview">
+                        
+
+                    </div>
                 </div>
-            </a>
+            {/key}
+
+
+
         </div>
 
 
@@ -54,6 +85,24 @@ CLOUDINARY_URL,
 </div>
 
 <style lang="scss">
+    @import '$lib/utils/css/magnifier.css';
+    .opaque {
+        opacity: .5;
+        background-color: #343232;
+    }
+    .original-img {
+        border: solid 1px#555;
+        text-align: center;
+        line-height: 30px;
+    }
+    #preview {
+        border: solid 1px#555;
+        text-align: center;
+        line-height: 30px;
+
+        height: 500px;
+        width: 500px;
+    }
     .modal_content {
         max-height:95%;
         width: 95%;
@@ -61,6 +110,17 @@ CLOUDINARY_URL,
             h1 {
                 text-align: center;
                 margin: auto;
+            }
+        }
+        .modal-body{ 
+            .content {
+                display:flex;
+                justify-content: center;
+                align-items: center;
+                .original-img {
+                    position: relative;
+                }
+
             }
         }
     }
