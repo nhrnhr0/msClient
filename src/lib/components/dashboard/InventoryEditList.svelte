3839ee8fa62fabe306f32195847970e10e6e4a24
nhrnhr0 <nhrnhr0@gmail.com>
2022-05-20 12:24:50 +0300

    adding grouping to yuval doc

--- src/lib/components/dashboard/InventoryEditList.svelte
+++ src/lib/components/dashboard/InventoryEditList.svelte
@@ -4,7 +4,10 @@
     import { onMount } from 'svelte';
     export let data;
     import {TabulatorFull as Tabulator} from 'tabulator-tables';
+import DragableEditList from '../DragableEditList.svelte';
 import StockEditPopup from './StockEditPopup.svelte';
+import {flip} from "svelte/animate";
+    import {dndzone} from "svelte-dnd-action";
     let tableDom;
 
     let table;
@@ -14,7 +17,16 @@ import StockEditPopup from './StockEditPopup.svelte';
     let id_stock_to_edit;
     let data_stock_to_edit;
     let show_stock_edit_popup;
-    
+
+    let HEADER_KEYS = ['id', 'warehouse_name', 'provider_name', 'product_name', 'barcode','color_name','size_name','verient_name','quantity','avgPrice',]
+    let HEADER_KEYS_WITH_IDS = HEADER_KEYS.map((v, index)=> {
+        return {
+            id: index,
+            name: v,
+        }
+    })
+
+
 
 
 
@@ -32,6 +44,7 @@ import StockEditPopup from './StockEditPopup.svelte';
             height:'auto', // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
             data:data, //assign data to table
             layout:"fitColumns", //fit columns to width of table (optional)
+            groupBy: ['warehouse_name', 'product_name'],
             columns:[ //Define Table Columns
                 {title:"מספר הכנסה", field:"id", sorter:"number", align:"center"},
                 
@@ -68,11 +81,73 @@ import StockEditPopup from './StockEditPopup.svelte';
     function closeStockEditPopup(){
         show_stock_edit_popup = false;
     }
+    /*
+    let selectedGropingKeys = [];
+    function headerKeyClicked(e) {
+        let key = {name: e.target.dataset.key,
+                    id: e.target.dataset.id,};
+
+        debugger;
+        let selectedGropingKeys2 = new Set(selectedGropingKeys);
+        selectedGropingKeys2.add(key);
+        selectedGropingKeys = [...new Set(selectedGropingKeys2)];
+
+    }
+*/
+    $: {
+        if(selectedKeys.length){
+            let vals = selectedKeys.map(key => key.name);
+            table.setGroupBy(vals);
+        }else if (table){
+            table.setGroupBy('');
+        }
+    }
+
+    const flipDurationMs = 300;
+    function handleDndConsider(e) {
+        HEADER_KEYS_WITH_IDS = e.detail.items;
+    }
+
+    function handleDndConsiderResult(e) {
+        selectedKeys = e.detail.items;
+    }
+
+    let selectedKeys = [];
+
 </script>
 <svelte:head>
     <link href="https://unpkg.com/tabulator-tables@5.2.4/dist/css/tabulator.min.css" rel="stylesheet">
 </svelte:head>
 <div>
+    <div class="headers-grouping">
+        <div class="dnd-zone" use:dndzone="{{items: HEADER_KEYS_WITH_IDS,}}" on:consider="{handleDndConsider}" on:finalize="{handleDndConsider}">
+            {#each HEADER_KEYS_WITH_IDS as item(item.id)}
+                <div class="drop-item">
+                    <div>{item.name}</div>
+                </div>
+            {/each}
+        </div>
+        <div class="dnd-zone" use:dndzone="{{items: selectedKeys,}}" on:consider="{handleDndConsiderResult}" on:finalize="{handleDndConsiderResult}">
+            {#each selectedKeys as item(item.id)}
+                <div class="drop-item">
+                    <div>{item.name}</div>
+                </div>
+            {/each}
+        </div>
+    <!--
+        <div class="headers-grouping-options">
+            {#each HEADER_KEYS as header,i}
+                <div class="headers-grouping-option">
+                    <input data-key={header} data-id={i} on:click="{headerKeyClicked}" type="checkbox" >
+                        {header}
+                </div>
+            {/each}
+        </div>
+        <div class="headers-droping-area">
+            <DragableEditList bind:list={selectedGropingKeys} />
+        </div>
+    -->
+    </div>
     <select id="filter-field-1">
         <option></option>
         <option value="created_at">נוצר ב</option>
@@ -101,6 +176,28 @@ import StockEditPopup from './StockEditPopup.svelte';
 <div bind:this={tableDom} id="example-table"></div>
 
 <style lang="scss">
+    .headers-grouping {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        .dnd-zone {
+            flex:1;
+            min-height: 150px;
+            border: 1px solid blue;
+
+            .drop-item {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 3px;
+                background-color: #d8d3d3;
+                margin: 10px;
+                border-radius: 25px;
+                cursor: move;
+            }
+        }
+    }
     //@import  "tabulator-tables";
     //@import  "~/tabulator-tables/dist/css/tabulator.min.css";
 </style>
