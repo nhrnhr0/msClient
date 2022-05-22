95befe9e93a595e0230719cec39ed88c7c6ab1dc
akashrajum7 <akashrajum7@gmail.com>
2022-05-22 19:31:42 +0530

    add cart history button in cart

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
-import { browser } from '$app/env';
+import { browser } from "$app/env";
 
 export let albumsJsonStore = writable([]);
 
@@ -8,9 +8,9 @@ export let categoryModalStore = writable();
 export let productModalStore = writable();
 export let productImageModalStore = writable();
 export let productQuestionModalStore = writable();
-export let loginModalStore= writable();
-export let userDetailModalStore= writable();
-export let productCartModalStore= writable();
+export let loginModalStore = writable();
+export let userDetailModalStore = writable();
+export let productCartModalStore = writable();
 export let productPhotoModalStore = writable();
 //export let singleAmountPopupStore= writable();
 export let _modal_z_index_incrementor = writable(0);
@@ -22,32 +22,44 @@ export let all_swipers = writable([]);
 
 export let successModalStore = writable();
 
+function createCartHistoryStore() {
+  const { subscribe, update } = writable({
+    showModal: false,
+  });
 
+  return {
+    subscribe,
+    openModal: () => update((state) => ({ ...state, showModal: true })),
+    closeModal: () => update((state) => ({ ...state, showModal: false })),
+  };
+}
 
+export const cartHistoryModalStore = createCartHistoryStore();
 
+export let cartHistoryModal = writable({
+  showModal: false,
+});
 
 // browser storage - userInfoStore
-let initUserData = {'isLogin': undefined};
-if(browser) {
-    if(typeof(Storage) !== "undefined") {
-        initUserData=JSON.parse(localStorage.getItem('user'));
-    }else{
-        initUserData=JSON.parse(sessionStorage.getItem('user'));
-    }
-    if(!initUserData) {
-        initUserData={'isLogin': undefined}
-    }
+let initUserData = { isLogin: undefined };
+if (browser) {
+  if (typeof Storage !== "undefined") {
+    initUserData = JSON.parse(localStorage.getItem("user"));
+  } else {
+    initUserData = JSON.parse(sessionStorage.getItem("user"));
+  }
+  if (!initUserData) {
+    initUserData = { isLogin: undefined };
+  }
 }
 export let userInfoStore = writable(initUserData);
 
 userInfoStore.subscribe((value) => {
-    if (browser) {
-        if(typeof(Storage) !== "undefined") {
-            window.localStorage.setItem('user', JSON.stringify(value));
-        }else {
-            window.sessionStorage.setItem('user', JSON.stringify(value));
-        }
+  if (browser) {
+    if (typeof Storage !== "undefined") {
+      window.localStorage.setItem("user", JSON.stringify(value));
+    } else {
+      window.sessionStorage.setItem("user", JSON.stringify(value));
     }
+  }
 });
-
-
